77203b3043236bc7d8b84a98db5a226ee18ccdf9
Carlos Ramirez <carlos@extrasistemas.com>
2024-04-03 23:02:16 -0600

    feat: add dtos and interfaces

--- src/cars/controllers/cars.controller.ts
+++ src/cars/controllers/cars.controller.ts
@@ -4,11 +4,12 @@ import {
   Delete,
   Get,
   Param,
-  ParseIntPipe,
+  ParseUUIDPipe,
   Patch,
   Post,
 } from '@nestjs/common';
 import { CarsService } from '../services/cars.service';
+import { CreateCarDto } from '../dtos';
 
 @Controller('cars')
 export class CarsController {
@@ -20,21 +21,21 @@ export class CarsController {
   }
 
   @Get(':id')
-  getOne(@Param('id', ParseIntPipe) id: number) {
+  getOne(@Param('id', new ParseUUIDPipe({ version: '4' })) id: string) {
     return this.carsService.findOne(id);
   }
   @Post()
-  createCar(@Body() body: any) {
+  createCar(@Body() body: CreateCarDto) {
     return body;
   }
 
   @Patch(':id')
-  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+  updateCar(@Param('id', ParseUUIDPipe) id: string, @Body() body: any) {
     return body;
   }
 
   @Delete(':id')
-  deleteCar(@Param('id', ParseIntPipe) id: number) {
+  deleteCar(@Param('id', ParseUUIDPipe) id: string) {
     return {
       method: 'delete',
       id,
--- src/cars/services/cars.service.ts
+++ src/cars/services/cars.service.ts
@@ -1,18 +1,19 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-
+import { v4 as uuid } from 'uuid';
+import { Car } from '../interfaces/car.interface';
 @Injectable()
 export class CarsService {
   private Cars = [
-    { id: 1, brand: 'Toyota', model: 'Corolla' },
-    { id: 2, brand: 'Toyota', model: 'Yaris' },
-    { id: 3, brand: 'Toyota', model: 'Camry' },
-    { id: 4, brand: 'Toyota', model: 'RAV4' },
+    { id: uuid(), brand: 'Toyota', model: 'Corolla' },
+    { id: uuid(), brand: 'Toyota', model: 'Yaris' },
+    { id: uuid(), brand: 'Toyota', model: 'Camry' },
+    { id: uuid(), brand: 'Toyota', model: 'RAV4' },
   ];
-  find() {
+  find(): Car[] {
     return this.Cars;
   }
 
-  findOne(id: number) {
+  findOne(id: string): Car {
     const car = this.Cars.find((r) => r.id === id);
     if (!car) throw new NotFoundException(`Car with id ${id} not found`);
     return car;
